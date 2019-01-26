cfc367229c773409b0a841a8720eae65b56d7483
Dustin277 <dd.badillo@gmail.com>
2019-01-26 11:01:59 -0800

    Update os.cpp

--- CSE460/Lab/Phase1/To Turn In/os.cpp
+++ CSE460/Lab/Phase1/To Turn In/os.cpp
@@ -3,75 +3,6 @@
 #include <string>
 #include <cstdlib>
 
-#include <Assembler.h>
-#include "VirtualMachine.h"
-using namespace std;
-
-int main(int argc, char *argv[])
-{
-  Assembler as;
-  VirtualMachine vm;
-
-  if (argc == 1){
-    cout << "Must supply an assembly file name. \n";
-    exit(1);
-  }
-
-  string assenblyFile = argv[1];
-  int pos = assemblyFile.find(".");
-  if (pos > asemblyFile.length() || assemblyFile.substr(pos) != ".s"{
-      cout << "No .s suffex.\n";
-      exit(2);
-    }
-
-    string name = assemblyFile.substr(0, pos);
-    string objectFile = name + ".0";
-
-    fstream assebly, objectCode;
-    assembly.open(assemblyFile.c_str(), ios::in);
-    objectCode.open(objectFile.c_str(), ios::out);
-    if (!assembly.is_open() or !objectCode.is_open()) {
-      cout << "Couldn't open" << assemblyFile << " and/or" << objectFile << endl;
-      exit(3);
-    }
-
-    if (as.assemble(assembly, objectCode)){
-      cout<< "Assembler Error\n";
-      objectCode.close();
-      exit(4);
-    }
-
-    assembly.close();
-    objectCode.close();
-
-    objectCode.open(objectFile.c_str(), ios::in);
-    if(!objectCode.is_open()){
-      cout << "Couldn't open " << objectFile << endl;
-      exit(5);
-    }
-
-    string inFile = name + ".in";
-    string outFile = name + ".out";
-    fstream in, out;
-    in.open(inFile.c_str(), ios::in);
-    out.open(outFile.c_str(),ios::out);
-    if (!in.is_open() or !out.is_open()){
-      cout << "Couldn't open" << inFile << "and/ or" << outFile << endl;
-
-    }
-
-    vm.run(objectCobe, in, out);
-    out << "Clock = " vm.get_clock() << endl;
-
-    objectCode.close();
-    in.close();
-    out.close();
-    return 0;
-}#include <fstream>
-#include <iostream>
-#include <string>
-#include <cstdlib>
-
 #include "Assembler.h"
 #include "VirtualMachine.h"
 
@@ -139,4 +70,4 @@ int main(int argc, char *argv[])
     out.close();
 
     return 0;
-} //main
+} // main
