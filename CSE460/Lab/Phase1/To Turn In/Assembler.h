fe31d1446bbe81367ccc3c904461467fc7d1f2cb
Dustin277 <dd.badillo@gmail.com>
2019-01-28 17:55:32 -0800

    Update Assembler.h

--- CSE460/Lab/Phase1/To Turn In/Assembler.h
+++ CSE460/Lab/Phase1/To Turn In/Assembler.h
@@ -1,3 +1,13 @@
+/*****************************************************************************
+Dustin Badillo, Bryan Soriano
+Jan 28, 2019
+Assembler.h
+PHASE1
+
+This file contains and implements the assembler class. 
+******************************************************************************/
+
+
 #ifndef ASSEMBLER_H
 #define ASSEMBLER_H
 
@@ -8,7 +18,7 @@ using namespace std;
 class Assembler {
 public:
     Assembler();
-    int assemble(fstream& in, fstream& out);
+    int assemble(fstream& in, fstream& out); //reads the file
 };
 
 #endif
