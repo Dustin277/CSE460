cef434d3b5274b60e17bd2cf89eb0dcbd34dd926
bsoriano21 <46697339+bsoriano21@users.noreply.github.com>
2019-02-17 03:28:16 -0800

    Update os.cpp
    OS::OS() still needs alot of editing

--- CSE460/Lab/Phase2/toturnin/os.cpp
+++ CSE460/Lab/Phase2/toturnin/os.cpp
@@ -4,7 +4,7 @@ Jan 28, 2019
 CSE 460
 Kay Zemoudeh 
 os.cpp
-PHASE2
+PHASE1
 
 This file is so far a rudimentary OS that so far and links the assembly files 
 as well as the virtual machine files to get an OS enviroment that recieves 
@@ -12,17 +12,17 @@ assembly files, translates them to an integer that is read by the virtual machin
 which in turn outputs the appropriate result as well as clock.
 ******************************************************************************/
 
-#include <fstream>
 #include <iostream>
-#include <string>
+#include <iomanip>
+#include <fstream>
 #include <cstdlib>
+#include <cassert>
 
 #include "Assembler.h"
 #include "VirtualMachine.h"
 #include "OS.h"
 
 using namespace std;
-
 
 OS::~OS(){ //write accounting
 
@@ -98,101 +98,166 @@ void OS::contextSwitch() // (4) switch(vm.ret)
 {
 
 }
- 
-OS::OS(){ // assemble/ load / create PCB
 
-	while(!in.eof()){
-	//When the OS comes up it looks in current directory and gathers all *.s files: 
-		system("ls *.s > progs") //prog is the string
-		progs.open("progs", ios::in) //opens the file
-	/*It then opens file progs and reads in file names. Each file is assembled,
-	its object code loaded in memory, and a pointer to its PCB is stored in a linked-list:*/ 
-		list<PCB *> jobs; //list of PCBs- jobs list
-		PCB * p = new PCB;
-		jobs.push_back(p);
-			
-	/* The processes (their PCBs) are either in ready, waiting,
-	   or running state. Maintain two queue of processes, Ready Queue 
-	   and Wait Queue, of type pointer to PCB:   */ 
-		queue<PCB *> readyQ, waitQ;
-
-	//  We also keep track of the running process by a pointer to its PCB:
-		PCB * running;
-	
-	//pushback all processes in readyq
-		
-		
-	} //end of while
-
-}//end of OS
-
-int main(int argc, char *argv[])
+OS::OS()
 {
+system ("ls *.s > progs");//reads files from directory
+progs.open("progs", ios::in); //allows for input
 
-while(!in.eof()){
-		Assembler as; //calls the assembler
-		VirtualMachine vm; //calls the virtual machine
+string assemblyFile;
+while (progs>>assemblyFile){		
+	Assembler as; //calls the assembler
+	VirtualMachine vm; //calls the virtual machine
 
-		if (argc == 1) { //checks if there was a file given (must be .s)
-			cout << "Must supply an assembly file name.\n";
-			exit(1);
-		}
+	/*if (argc == 1) { //checks if there was a file given (must be .s)
+		cout << "Must supply an assembly file name.\n";
+		exit(1);
+	}*/
+ 
+	int pos = assemblyFile.find("."); //looks 460/for the "." for start of file
+	//checks to make sure that the file is an assembly file
+	if (pos > assemblyFile.length() || assemblyFile.substr(pos) != ".s") { 
+		cout << "No .s suffix.\n";
+		exit(2);
+	}
 
-		string assemblyFile = argv[1]; 
-		int pos = assemblyFile.find("."); //looks for the "." for start of file
-		//checks to make sure that the file is an assembly file
-		if (pos > assemblyFile.length() || assemblyFile.substr(pos) != ".s") { 
-			cout << "No .s suffix.\n";
-			exit(2);
-		}
+	string name = assemblyFile.substr(0, pos); 
+	string objectFile = name + ".o"; 
 
-		string name = assemblyFile.substr(0, pos); 
-		string objectFile = name + ".o"; 
+	fstream assembly, objectCode;
+	assembly.open(assemblyFile.c_str(), ios::in);
+	objectCode.open(objectFile.c_str(), ios::out);
+	if (!assembly.is_open() or !objectCode.is_open()) {
+		cout << "Couldn't open " << assemblyFile << " and/or " << objectFile << endl;
+		exit(3);
+	}
 
-		fstream assembly, objectCode;
-		assembly.open(assemblyFile.c_str(), ios::in);
-		objectCode.open(objectFile.c_str(), ios::out);
-		if (!assembly.is_open() or !objectCode.is_open()) {
-			cout << "Couldn't open " << assemblyFile << " and/or " << objectFile << endl;
-			exit(3);
-		}
-
-		if (as.assemble(assembly, objectCode)) { //checks if opcode from assembly file is valid
-			cout << "Assembler Error\n";
-			assembly.close();
-			objectCode.close();
-			exit(4);
-		}
-
+	if (as.assemble(assembly, objectCode)) { //checks if opcode from assembly file is valid
+		cout << "Assembler Error\n";
 		assembly.close();
 		objectCode.close();
+		exit(4);
+	}
 
-		objectCode.open(objectFile.c_str(), ios::in);
-		if (!objectCode.is_open()) {
-			cout << "Couldn't open " << objectFile << endl;
-			exit(5);
-		}
-		
-		string inFile = name + ".in"; //input file for assembly code
-		string outFile = name + ".out"; //output result
-		fstream in, out;
-		in.open(inFile.c_str(), ios::in);
-		out.open(outFile.c_str(), ios::out);
-		//check if either the in file or out file are not opening
-		if (!in.is_open() or !out.is_open()) { 
-			cout << "Couldn't open " << inFile << " and/or " << outFile << endl;
-			exit(6);
-		}
+	assembly.close();
+	objectCode.close();
 
-		vm.run(objectCode, in, out);
-		out << "Clock = " << vm.get_clock() << endl; //outputs clock
+	objectCode.open(objectFile.c_str(), ios::in);
+	if (!objectCode.is_open()) {
+		cout << "Couldn't open " << objectFile << endl;
+		exit(5);
+	}
+	
+	string inFile = name + ".in"; //input file for assembly code
+	string outFile = name + ".out"; //output result
+	fstream in, out;
+	in.open(inFile.c_str(), ios::in);
+	out.open(outFile.c_str(), ios::out);
+	//check if either the in file or out file are not opening
+	if (!in.is_open() or !out.is_open()) { 
+		cout << "Couldn't open " << inFile << " and/or " << outFile << endl;
+		exit(6);
+	}
 
+	vm.run(objectCode, in, out);
+	out << "Clock = " << vm.get_clock() << endl; //outputs clock
+
+	objectCode.close();
+	in.close();
+	out.close();
+
+	list<PCB *> jobs;
+	PCB * p = new PCB;
+	jobs.push_back(p);
+}
+}
+/*queue<PCB *> readyQ, waitQ;//making a queue for ready and wait of type PCB containing pointers
+
+PCB * running;//keeps track of running process
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*int main(int argc, char *argv[])
+{
+	Assembler as; //calls the assembler
+	VirtualMachine vm; //calls the virtual machine
+
+	if (argc == 1) { //checks if there was a file igiven (must be .s)
+		cout << "Must supply an assembly file name.\n";
+		exit(1);
+	}
+
+	string assemblyFile = argv[1]; 
+	int pos = assemblyFile.find("."); //looks 460/for the "." for start of file
+	//checks to make sure that the file is an assembly file
+	if (pos > assemblyFile.length() || assemblyFile.substr(pos) != ".s") { 
+		cout << "No .s suffix.\n";
+		exit(2);
+	}
+
+	string name = assemblyFile.substr(0, pos); 
+	string objectFile = name + ".o"; 
+
+	fstream assembly, objectCode;
+	assembly.open(assemblyFile.c_str(), ios::in);
+	objectCode.open(objectFile.c_str(), ios::out);
+	if (!assembly.is_open() or !objectCode.is_open()) {
+		cout << "Couldn't open " << assemblyFile << " and/or " << objectFile << endl;
+		exit(3);
+	}
+
+	if (as.assemble(assembly, objectCode)) { //checks if opcode from assembly file is valid
+		cout << "Assembler Error\n";
+		assembly.close();
 		objectCode.close();
-		in.close();
-		out.close();
-		
-		}
-		return 0;
-	} // main
+		exit(4);
+	}
 
+	assembly.close();
+	objectCode.close();
+
+	objectCode.open(objectFile.c_str(), ios::in);
+	if (!objectCode.is_open()) {
+		cout << "Couldn't open " << objectFile << endl;
+		exit(5);
+	}
+	
+	string inFile = name + ".in"; //input file for assembly code
+	string outFile = name + ".out"; //output result
+	fstream in, out;
+	in.open(inFile.c_str(), ios::in);
+	out.open(outFile.c_str(), ios::out);
+	//check if either the in file or out file are not opening
+	if (!in.is_open() or !out.is_open()) { 
+		cout << "Couldn't open " << inFile << " and/or " << outFile << endl;
+		exit(6);
+	}
+
+	vm.run(objectCode, in, out);
+	out << "Clock = " << vm.get_clock() << endl; //outputs clock
+
+	objectCode.close();
+	in.close();
+	out.close();
+
+	return 0;
+} // main */
 
