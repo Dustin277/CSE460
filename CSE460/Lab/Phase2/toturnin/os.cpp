053de09727002a4de4d893471f084c61ce67ead9
bsoriano21 <46697339+bsoriano21@users.noreply.github.com>
2019-02-18 02:33:45 -0800

    Update os.cpp

--- CSE460/Lab/Phase2/toturnin/os.cpp
+++ CSE460/Lab/Phase2/toturnin/os.cpp
@@ -16,7 +16,6 @@ which in turn outputs the appropriate result as well as clock.
 #include <iomanip>
 #include <fstream>
 #include <cstdlib>
-#include <cassert>
 
 #include "Assembler.h"
 #include "VirtualMachine.h"
@@ -25,6 +24,28 @@ which in turn outputs the appropriate result as well as clock.
 using namespace std;
 
 OS::~OS() { //write accounting
+list<PCB *>::iterator ptr;// creating an iterator
+
+    int cpu_time = 0;
+    for (ptr = jobs.begin(); ptr != jobs.end(); ptr++)//incremanting cpu time for every jon untill complete
+        cpu_time += (*ptr)->cpu_time;
+
+    for (ptr = jobs.begin(); ptr != jobs.end(); ptr++) {//updating all information for ever job.
+        (*ptr)->out << "Turn around time = " << (*ptr)->turn_around_time << ", CPU time = " << (*ptr)->cpu_time
+              << ", Wait time = " << (*ptr)->wait_time << ", IO time = " << (*ptr)->io_time << endl;
+
+        (*ptr)->out << "Total CPU time = " << cpu_time << ", System time = " << sys_time
+              << ", Idle time = " << idle_time << ", Final clock = " << vm.clock << endl
+              << "Real CPU Utilization = " << setprecision(3) /*sets decimal precision for float*/ 
+	      << (float) cpu_time / vm.clock * 100 << "%, "
+              << "System CPU Utilization = " << (float) (vm.clock - idle_time) / vm.clock * 100 << "%, "
+              << "Throughput = " << jobs.size() / ((float) vm.clock / 1000) << endl << endl;
+
+        (*ptr)->in.close();
+        (*ptr)->out.close();
+        (*ptr)->stack.close();
+    }
+    progs.close();
 
 }
 
@@ -96,6 +117,68 @@ void OS::saveState() //(3)save vm => running
 
 void OS::contextSwitch() // (4) switch(vm.ret)
 {
+	while (!waitQ.empty() && waitQ.front()->io_completion <= vm.clock) {
+        readyQ.push(waitQ.front());
+        waitQ.front()->wait_time_begin = vm.clock;
+        waitQ.front()->io_time += (vm.clock - waitQ.front()->io_time_begin);
+        waitQ.pop();
+    }
+
+    int vm_status = (vm.sr >> 5) & 07;//not sure waht is does...
+    switch (vm_status) {
+        case 0: //Time slice
+            readyQ.push(running);
+            running->wait_time_begin = vm.clock;
+            break;
+        case 1: //Halt
+            running->out << running->prog << ": Terminated\n";
+            running->turn_around_time = vm.clock;
+            break;
+        case 2: //Out of Bound Error
+            running->out << running->prog << ": Out of bound Error, pc = " << vm.pc << endl;
+            running->turn_around_time = vm.clock;
+            break;
+        case 3: //Stack Overflow
+            running->out << running->prog << ": Stack overflow, pc = " << vm.pc << ", sp = " << vm.sp << endl;
+            running->turn_around_time = vm.clock;
+            break;
+        case 4: //Stack Underflow
+            running->out << running->prog << ": Stack underflow, pc = " << vm.pc << ", sp = " << vm.sp << endl;
+            running->turn_around_time = vm.clock;
+            break;
+        case 5: //Bad Opcode
+            running->out << running->prog << ": Bad opcode, pc = " << vm.pc << endl; 
+            running->turn_around_time = vm.clock;
+            break;
+        case 6: //Read
+        case 7: //Write
+            waitQ.push(running);
+            running->io_completion = vm.clock + 27;
+            running->io_time_begin = vm.clock;
+            break;
+        default:
+            cerr << running->prog << ": Unexpected status = " << vm_status 
+                 << " pc = " << vm.pc << " time = " << vm.clock << endl;
+            running->out << running->prog << ": Unexpected status: " << vm_status 
+                 << " pc = " << vm.pc << " time = " << vm.clock << endl;
+            running->turn_around_time = vm.clock;
+    }
+
+    saveState();
+    running = 0;
+    if (not readyQ.empty()) {
+        running = readyQ.front();
+        running->wait_time += (vm.clock - running->wait_time_begin);
+        readyQ.pop();
+    } 
+    else if (not waitQ.empty()){
+        running = waitQ.front();
+        waitQ.pop();
+        idle_time += (running->io_completion - vm.clock);
+        vm.clock = running->io_completion;
+        // assume all of context switch time is incurred after idle time
+        running->io_time += (vm.clock - running->io_time_begin);
+    }
 
 }
 
@@ -103,161 +186,49 @@ OS::OS()
 {
 	system("ls *.s > progs");//reads files from directory
 	progs.open("progs", ios::in); //allows for input
+	
+	int base = 0;
+	int limit;
 
 	string assemblyFile;
-	while (progs >> assemblyFile) {
-		Assembler as; //calls the assembler
-		VirtualMachine vm; //calls the virtual machine
-
-						   /*if (argc == 1) { //checks if there was a file given (must be .s)
-						   cout << "Must supply an assembly file name.\n";
-						   exit(1);
-						   }*/
-
-		int pos = assemblyFile.find("."); //looks 460/for the "." for start of file
-										  //checks to make sure that the file is an assembly file
-		if (pos > assemblyFile.length() || assemblyFile.substr(pos) != ".s") {
-			cout << "No .s suffix.\n";
-			exit(2);
-		}
-
-		string name = assemblyFile.substr(0, pos);
-		string objectFile = name + ".o";
-
+	while (progs >> assemblyFile) {//while there is a .s file going in
 		fstream assembly, objectCode;
-		assembly.open(assemblyFile.c_str(), ios::in);
-		objectCode.open(objectFile.c_str(), ios::out);
-		if (!assembly.is_open() or !objectCode.is_open()) {
-			cout << "Couldn't open " << assemblyFile << " and/or " << objectFile << endl;
-			exit(3);
-		}
+		int pos = assemblyFile.find("."); //looks for the "." for start of file
+		string name = assemblyFile.substr(0, pos);//name of the file which ends before the "."
 
+		assembly.open(assemblyFile.c_str(), ios::in);
+		objectCode.open((name + ".o").c_str(), ios::out);//opens corresponding ".o" file.
+		if (!assembly.is_open() or !objectCode.is_open()) {
+			cout << "Couldn't open" << endl;
+			//exit(3);
+		}
 		if (as.assemble(assembly, objectCode)) { //checks if opcode from assembly file is valid
 			cout << "Assembler Error\n";
 			assembly.close();
 			objectCode.close();
-			exit(4);
+			continue;
+			//exit(4);
 		}
-
 		assembly.close();
 		objectCode.close();
+		objectCode.open((name+".o").c_str(), ios::in);
+        	//need to check if it open
+        	vm.load(objectCode, base, limit);
+        	objectCode.close();
 
-		objectCode.open(objectFile.c_str(), ios::in);
-		if (!objectCode.is_open()) {
-			cout << "Couldn't open " << objectFile << endl;
-			exit(5);
-		}
 
-		string inFile = name + ".in"; //input file for assembly code
-		string outFile = name + ".out"; //output result
-		fstream in, out;
-		in.open(inFile.c_str(), ios::in);
-		out.open(outFile.c_str(), ios::out);
-		//check if either the in file or out file are not opening
-		if (!in.is_open() or !out.is_open()) {
-			cout << "Couldn't open " << inFile << " and/or " << outFile << endl;
-			exit(6);
-		}
-
-		vm.run(TIME_SLICE, in, out);
-		out << "Clock = " << vm.get_clock() << endl; //outputs clock
-
-		objectCode.close();
-		in.close();
-		out.close();
-
-		list<PCB *> jobs;
-		PCB * p = new PCB;
-		jobs.push_back(p);
+		PCB * job = new PCB(assemblyFile, base, limit - base);
+        	job->in.open((name+".in").c_str(), ios::in);
+        	job->out.open((name+".out").c_str(), ios::out);
+        	job->stack.open((name+".st").c_str(), ios::in | ios::out);
+		
+		jobs.push_back(job);
+		base = limit;
+	}
+	vm.total_limit = limit;
+	list<PCB *>::iterator ptr;
+	
+	for ( ptr = jobs.begin(); ptr != jobs.end(); ptr++){
+		readyQ.push(*ptr);//pushing next job on the list to readyQ.
 	}
 }
-/*queue<PCB *> readyQ, waitQ;//making a queue for ready and wait of type PCB containing pointers
-
-PCB * running;//keeps track of running process
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*int main(int argc, char *argv[])
-{
-Assembler as; //calls the assembler
-VirtualMachine vm; //calls the virtual machine
-
-if (argc == 1) { //checks if there was a file igiven (must be .s)
-cout << "Must supply an assembly file name.\n";
-exit(1);
-}
-
-string assemblyFile = argv[1];
-int pos = assemblyFile.find("."); //looks 460/for the "." for start of file
-//checks to make sure that the file is an assembly file
-if (pos > assemblyFile.length() || assemblyFile.substr(pos) != ".s") {
-cout << "No .s suffix.\n";
-exit(2);
-}
-
-string name = assemblyFile.substr(0, pos);
-string objectFile = name + ".o";
-
-fstream assembly, objectCode;
-assembly.open(assemblyFile.c_str(), ios::in);
-objectCode.open(objectFile.c_str(), ios::out);
-if (!assembly.is_open() or !objectCode.is_open()) {
-cout << "Couldn't open " << assemblyFile << " and/or " << objectFile << endl;
-exit(3);
-}
-
-if (as.assemble(assembly, objectCode)) { //checks if opcode from assembly file is valid
-cout << "Assembler Error\n";
-assembly.close();
-objectCode.close();
-exit(4);
-}
-
-assembly.close();
-objectCode.close();
-
-objectCode.open(objectFile.c_str(), ios::in);
-if (!objectCode.is_open()) {
-cout << "Couldn't open " << objectFile << endl;
-exit(5);
-}
-
-string inFile = name + ".in"; //input file for assembly code
-string outFile = name + ".out"; //output result
-fstream in, out;
-in.open(inFile.c_str(), ios::in);
-out.open(outFile.c_str(), ios::out);
-//check if either the in file or out file are not opening
-if (!in.is_open() or !out.is_open()) {
-cout << "Couldn't open " << inFile << " and/or " << outFile << endl;
-exit(6);
-}
-
-vm.run(objectCode, in, out);
-out << "Clock = " << vm.get_clock() << endl; //outputs clock
-
-objectCode.close();
-in.close();
-out.close();
-
-return 0;
-} // main */
-
